2e1e76221cbbe5016febbcaaad3511f365c5d419
lulo93 <94081621+lulo93@users.noreply.github.com>
2021-12-10 23:28:32 +0000

    removed console log

--- src/app.js
+++ src/app.js
@@ -70,7 +70,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "24be2a5d0560b29769b34e4136c9cb1f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
